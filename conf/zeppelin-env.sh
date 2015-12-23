5ec6908481055396f696e74a0fac7cc8518ddfd0
Andrey Shevchenko <ashevchenko@cogniance.com>
2015-12-23 14:07:27 +0200

    - Added Elodina Streaming  and Elodina dashboard notebook for realtime metrics - Removed sample Elodina notebook - Configured Spark to use Coarse-grained scheduler in Mesos

--- conf/zeppelin-env.sh
+++ conf/zeppelin-env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export SPARK_HOME=/tmp/spark-1.5.1-bin-hadoop2.3
-export SPARK_SUBMIT_OPTIONS="--master mesos://leader.mesos.service:5050 --conf spark.mesos.executor.home=/tmp/spark-1.5.1-bin-hadoop2.3"
+export SPARK_HOME=/opt/spark
+export SPARK_SUBMIT_OPTIONS="--master mesos://leader.mesos.service:5050 --conf spark.mesos.executor.home=/opt/spark --conf spark.mesos.coarse=true"
 
