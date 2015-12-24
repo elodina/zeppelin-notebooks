7d808766db3e1d75892d59c39accea015e9e48d8
Andrey Shevchenko <ashevchenko@cogniance.com>
2015-12-24 15:15:11 +0200

    Added Cassandra support in dedicated interpreter and Spark connector. Added dedicated notebook for loading notebook dependencies

--- conf/zeppelin-env.sh
+++ conf/zeppelin-env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 export SPARK_HOME=/opt/spark
-export SPARK_SUBMIT_OPTIONS="--master mesos://leader.mesos.service:5050 --conf spark.mesos.executor.home=/opt/spark --conf spark.mesos.coarse=true"
+export SPARK_SUBMIT_OPTIONS="--master mesos://leader.mesos.service:5050 --conf spark.mesos.executor.home=/opt/spark --conf spark.mesos.coarse=true --conf spark.cassandra.connection.host=cassandra-node-0.service"
 
