a1cb596412573d8b0e057c9459043c050f2ee534
Oleg Kovalenko <olegvk8@gmail.com>
2016-01-04 19:03:37 +0200

    Use less mem in marathon json and restrict resources in zeppelin env.
    DRIVER_MEMORY=1536M is xmx for driver program (note: it is part of
    all memory allocated by marathon json for zeppelin).
    
    EXECUTOR_MEMORY="2560M" is memory that will be booked by executor
    on each mesos agent (+10% or 384M, see spark.mesos.executor.memoryOverhead)
    
    CORES_MAX=6 restrict to use at most 6 cores across all
    executors (otherwise spark will allocate all available
    cores that mesos offers)
    
    Using coarse mode.
    
    ZEPPELIN_MEM="-Xmx1024m -XX:MaxPermSize=512m" zeppelin server mem
    
    ZEPPELIN_INTP_MEM="-Xmx$DRIVER_MEMORY -XX:MaxPermSize=512m" use as much
    as driver memory
    
    ZEPPELIN_JAVA_OPTS configures
    - spark executor memory
    - driver's memory (becuase zeppelin server launches driver as subprocess)
    - executor's memory
    - restrict num of cores to use across all executors (via CORES_MAX)
    - changes spark ui port to PORT0 or default 4040

--- conf/zeppelin-env.sh
+++ conf/zeppelin-env.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 export SPARK_HOME=/opt/spark
-export SPARK_SUBMIT_OPTIONS="--master mesos://leader.mesos.service:5050 --conf spark.mesos.executor.home=/opt/spark --conf spark.mesos.coarse=true --conf spark.cassandra.connection.host=cassandra-node-0.service"
-
+export DRIVER_MEMORY="1536M"
+export EXECUTOR_MEMORY="2560M"
+export CORES_MAX=6
+export SPARK_SUBMIT_OPTIONS="--master ${SPARK_MESOS_MASTER-mesos://leader.mesos.service:5050} --conf spark.logConf=true --conf spark.mesos.executor.home=$SPARK_HOME --conf spark.mesos.coarse=true --conf spark.cassandra.connection.host=cassandra-node-0.service"
+export ZEPPELIN_MEM="-Xmx1024m -XX:MaxPermSize=512m"
+export ZEPPELIN_INTP_MEM="-Xmx$DRIVER_MEMORY -XX:MaxPermSize=512m"
+export ZEPPELIN_JAVA_OPTS="-Dspark.home=$SPARK_HOME -Dspark.driver.memory=$DRIVER_MEMORY -Dspark.executor.memory=$EXECUTOR_MEMORY -Dspark.cores.max=$CORES_MAX -Dspark.ui.port=${PORT0-4040}"
